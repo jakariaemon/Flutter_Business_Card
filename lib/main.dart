1e2d8110fdca7d9508614ed9627b26e8c650e351
“[Jakaria <[jakariaszlzmmz12@gmail.com]>
2021-12-10 19:58:44 +0600

    Container, Paddiing, Mergin, Safe Area

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,10 +15,18 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         appBar: AppBar(
-          title: Center(child: Text('My App')),
+          title: Center(child: Text('Business Card')),
           backgroundColor: Colors.blueGrey[900],
         ),
-        body: Center(
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 150.0), // for outer spacing
+            //padding: EdgeInsets.only(top:50), // for inner spacing of the contents
+            color: Colors.white,
+            child: Center(child: Text("Emon")),
+          ),
         ),
       ),
     );

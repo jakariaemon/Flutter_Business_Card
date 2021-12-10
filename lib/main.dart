b0d5324329c8a7fd1524d9e6941dec15800d3c54
“[Jakaria <[jakariaszlzmmz12@gmail.com]>
2021-12-10 21:12:25 +0600

    Colum

--- lib/main.dart
+++ lib/main.dart
@@ -19,18 +19,43 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blueGrey[900],
         ),
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 150.0), // for outer spacing
-            //padding: EdgeInsets.only(top:50), // for inner spacing of the contents
-            color: Colors.white,
-            child: Center(child: Text("Emon")),
-          ),
-        ),
+          child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget> [
+              Container(
+                height: 100.0,
+                width: 100.0,
+                //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 150.0), // for outer spacing
+                margin: EdgeInsets.only(bottom: 10.0),
+                //padding: EdgeInsets.only(top:50), // for inner spacing of the contents
+                color: Colors.white,
+                child: Center(child: Text("Emon")),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                margin: EdgeInsets.only(bottom: 10.0),
+                //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 150.0), // for outer spacing
+                //padding: EdgeInsets.only(top:50), // for inner spacing of the contents
+                color: Colors.red,
+                child: Center(child: Text("Emon")),
+              ),
+               Container(
+                height: 100.0,
+                width: 100.0,
+                margin: EdgeInsets.only(bottom: 10.0),
+               //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 150.0), // for outer spacing
+               //padding: EdgeInsets.only(top:50), // for inner spacing of the contents
+                color: Colors.blue,
+                child: Center(child: Text("Emon")),
+               ),
+            ],
+          )
       ),
+    ),
     );
   }
 }
 
 
+// Flutter Cheatsheet: https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e
